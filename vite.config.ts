92b2be295f6a0088ec0bc515185d8c53c8da8b99
Luka <bosnjakluka008@gmail.com>
2026-05-02 13:50:52 +0200

    404 error fix part 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/my-site/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                photos: resolve(__dirname, 'photos.html'),
+                projects: resolve(__dirname, 'projects.html'),
+            }
+        }
+    }
 })
